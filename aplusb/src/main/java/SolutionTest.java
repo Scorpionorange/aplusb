1b061e8fcb8b1643806dfa0ec76d50f52188f1df
ScorpionOrange <scorpionorange@126.com>
2016-10-13 00:13:58 +0800

    Initial commit

---
+++ aplusb/src/main/java/SolutionTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ScorpionOrange on 2016/10/07.
+ */
+public class SolutionTest {
+    public static void main(String[] args){
+        System.out.println("A plus B (3 + 5) = " + new Solution().aplusb(3, 5));
+    }
+}
